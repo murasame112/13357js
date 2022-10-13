21bbe8081efe17a4165e5a55526427ac6a2d188a
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-10-13 15:10:15 +0200

    zajecia 13.10

--- projekt2/script.js
+++ projekt2/script.js
@@ -1,8 +1,35 @@
-//zdjecia mozna klikac
-// clearInterval/clearTimeout
-// setTimeout(
-//     () => {
-//         MediaDeviceInfo.innerHtml = "set";
-//     }, 2000
-// )
-// malo animacji w js robic, raczej glownie w js
+window.addEventListener('DOMContentLoaded', (event) => {
+    const slides = document.querySelectorAll('.slide');   
+    const slideCount = document.querySelectorAll('.slide').length;
+    const button = document.querySelector('#next');
+
+
+    button.addEventListener('click', () => {
+        let slideId = getMainSlideId();
+        const mainSlide = document.querySelector('.main_slide');
+        slideId++;
+        const newSlide = document.querySelector('#slide' + slideId + '');
+        slideId++;
+        const nextSlide = document.querySelector('#slide' + slideId + '');
+        const subSlides = document.querySelectorAll('.sub_slide');
+        
+        mainSlide.classList.remove('main_slide');
+        mainSlide.classList.add('sub_slide');
+        
+        subSlides.forEach(function(elem) {
+            elem.classList.remove('sub_slide');
+        });
+
+        newSlide.classList.add('main_slide');
+
+        nextSlide.classList.add('sub_slide');
+    });
+
+});
+
+function getMainSlideId(){
+    slide = document.querySelector('.main_slide');
+    let id = slide.id;
+    id = id.slice(5);
+    return id; 
+}
