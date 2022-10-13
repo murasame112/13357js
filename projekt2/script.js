bd48b3be33ba85d2df15bbecd27720d1fbb59302
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-10-13 13:56:07 +0200

    initial commit po obecnosc

---
+++ projekt2/script.js
@@ -0,0 +1,8 @@
+//zdjecia mozna klikac
+// clearInterval/clearTimeout
+// setTimeout(
+//     () => {
+//         MediaDeviceInfo.innerHtml = "set";
+//     }, 2000
+// )
+// malo animacji w js robic, raczej glownie w js
