caaa78a3728226a48ec0f196806643a0ac8a4839
murasame112 <79984587+murasame112@users.noreply.github.com>
2022-10-17 11:15:48 +0200

    wiecej przyciskow

--- projekt2/script.js
+++ projekt2/script.js
@@ -1,31 +1,81 @@
+let playSlidesInterval;
+let slideCount
 window.addEventListener('DOMContentLoaded', (event) => {
     const slides = document.querySelectorAll('.slide');   
-    const slideCount = document.querySelectorAll('.slide').length;
-    const button = document.querySelector('#next');
+    slideCount = document.querySelectorAll('.slide').length;
+    const buttonNext = document.querySelector('#next');
+    const buttonPrev = document.querySelector('#prev');
+    const buttonPlay = document.querySelector('#play');
+    const buttonStop = document.querySelector('#stop');
 
+    //playSlidesInterval = setInterval(moveRight, 3000);
 
-    button.addEventListener('click', () => {
-        let slideId = getMainSlideId();
-        const mainSlide = document.querySelector('.main_slide');
-        slideId++;
-        const newSlide = document.querySelector('#slide' + slideId + '');
-        slideId++;
-        const nextSlide = document.querySelector('#slide' + slideId + '');
-        const subSlides = document.querySelectorAll('.sub_slide');
-        
-        mainSlide.classList.remove('main_slide');
-        mainSlide.classList.add('sub_slide');
-        
-        subSlides.forEach(function(elem) {
-            elem.classList.remove('sub_slide');
-        });
+    buttonNext.addEventListener('click', moveRight);
+    buttonPrev.addEventListener('click', moveLeft);
+    buttonPlay.addEventListener('click', playSlides);
+    buttonStop.addEventListener('click', stopSlides);
 
-        newSlide.classList.add('main_slide');
-
-        nextSlide.classList.add('sub_slide');
-    });
 
 });
+
+function moveRight(){
+    let slideId = getMainSlideId();
+    if(slideId == slideCount-1){
+        rerollRight();
+        return 0;
+    }
+    const mainSlide = document.querySelector('.main_slide');
+    slideId++;
+    const newSlide = document.querySelector('#slide' + slideId + '');
+    slideId++;
+    const nextSlide = document.querySelector('#slide' + slideId + '');
+    const subSlides = document.querySelectorAll('.sub_slide');
+    
+    mainSlide.classList.remove('main_slide');
+    mainSlide.classList.add('sub_slide');
+    
+    subSlides.forEach(function(elem) {
+        elem.classList.remove('sub_slide');
+    });
+
+    newSlide.classList.add('main_slide');
+
+    nextSlide.classList.add('sub_slide');
+}
+
+function moveLeft(){
+    let slideId = getMainSlideId();
+    const mainSlide = document.querySelector('.main_slide');
+    slideId--;
+    const newSlide = document.querySelector('#slide' + slideId + '');
+    slideId--;
+    const nextSlide = document.querySelector('#slide' + slideId + '');
+    const subSlides = document.querySelectorAll('.sub_slide');
+    
+    mainSlide.classList.remove('main_slide');
+    mainSlide.classList.add('sub_slide');
+    
+    subSlides.forEach(function(elem) {
+        elem.classList.remove('sub_slide');
+    });
+
+    newSlide.classList.add('main_slide');
+
+    nextSlide.classList.add('sub_slide');
+}
+
+function playSlides(){
+    playSlidesInterval = setInterval(moveRight, 3000);
+}
+
+function stopSlides(){
+    clearInterval(playSlidesInterval);
+}
+
+function rerollRight(){
+    console.log("sadf");
+    
+}
 
 function getMainSlideId(){
     slide = document.querySelector('.main_slide');
