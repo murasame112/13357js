7c38567a9c2a5ba516286db265dc9d3ed1d753f6
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-11-10 15:08:15 +0100

    todo1

--- projekt5 - coupling/script.js
+++ projekt5 - coupling/script.js
@@ -1,23 +1,77 @@
-let arr = [saveCToSessionStorage(5),discoverPowerBallNumber(5)]
-interval(arr);
 
-function interval(data) {
-  let timer = 1
-  setInterval(
-    () => {
-      // mamy coupling - interval ma na sztywno zaszyte w sobie C i D (..i logger)
-      data.forEach(element => {
-          element();
-      });
-      timer++
+// interval(arr);
+
+class FuncLibrary{
+    constructor(array){
+        if(array != null){
+            this.container = array;
+        }else{
+            this.container = [];
+        }
+        this.subscribers = [];
     }
-    , 2000)
+
+    subscribe(subscriber){
+        this.subscribers.push(subscriber);
+    }
+
+    notify(){
+        this.subscribers.forEach(element => {
+            element.update(this.container);
+        });
+    }
+
+    addToLibrary(callback){
+        this.container.push(callback);
+        this.notify();
+    }
+
 }
+
+class Subscriber{
+
+    constructor(){
+        this.functions = [];        
+    }
+
+    update(funcArray){
+        funcArray.forEach(element => {
+            this.functions.push(element);
+        })
+    }
+
+}
+
+let publisher = new FuncLibrary();
+let subscriber = new Subscriber();
+publisher.subscribe(subscriber);
+publisher.addToLibrary(saveCToSessionStorage);
+publisher.addToLibrary(discoverPowerBallNumber);
+
+function interval(){
+    let timer = 1;
+    let arr = subscriber.functions;
+    setInterval(
+        () => {
+        // mamy coupling - interval ma na sztywno zaszyte w sobie C i D (..i logger)
+        arr.forEach(element => {
+            element(timer);
+        });
+        timer++;
+        }
+        , 2000);
+}
+
+interval();
+
 
 class Logger {
   static log(data) {
     console.log(data)
   }
+
+  
+
 }
 
 function saveCToSessionStorage(data) {
