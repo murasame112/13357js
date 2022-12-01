706f66f29c79ad7c6529f6f1addad7029bf0013a
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-12-01 15:15:34 +0100

    koniec zajec 01.12

--- projekt8/script.js
+++ projekt8/script.js
@@ -3,11 +3,13 @@
 
 // todo1 - kulki nie moga sie respic na innych kulkach
 // todo2 - guzik "draw" jest wyszarzony dopóki nie ma wartości X wpisanej, a potem jesli ktos ja usunie lub go zwyczajnie kliknie
+// todo3 - przeniesc generowanie kulek do init() z draw()
 
 const canvas = document.querySelector('#canva');
 const ctx = canvas.getContext('2d');
 
 const drawButton = document.querySelector('#draw');
+const initButton = document.querySelector('#init');
 const xValue = document.querySelector('#xValue');
 
 xValue.addEventListener('change', setDraw);
@@ -15,32 +17,71 @@ xValue.addEventListener('change', setDraw);
 function setDraw(){
     if(xValue.value.length != 0){
         // todo2 od-szarzanie buttona
-        drawButton.addEventListener('click', draw);
+        initButton.addEventListener('click', init);
     }else{
         // todo2 wyszarzanie buttona
     }
 }
 
+drawButton.addEventListener('click', ()=>{setInterval(draw,1000)});
+const radius = 10;
+const maxWidth = 480;
+const minWidth = 20;
 
 
-const maxWidth = 475;
-const minWidth = 25;
+function init(){
+    
 
+    
+}
 
-  
 
 
 function draw() {
-    for(let i = 0; i < xValue.value; i++){
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //for(let i = 0; i < xValue.value; i++){
         let width = Math.random() * (maxWidth - minWidth) + minWidth;
         let height = Math.random() * (maxWidth - minWidth) + minWidth;
         if (canvas.getContext) {
             ctx.beginPath();
-            ctx.arc(width, height, 10, 0, 2 * Math.PI);
+            ctx.arc(width, height, radius, 0, 2 * Math.PI);
+            ctx.fillStyle = '#49d169';
+            ctx.fill();
             ctx.stroke();
-    
+            ctx.save();
         }
-    }
+    //}
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    ctx.stroke();
+    
+    
+    //ctx.translate(5, 5);
+    
+
+    //ctx.restore();
+  
+    // // Earth
+    // const time = new Date();
+    // ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+    // ctx.translate(105, 0);
+    // ctx.fillRect(0, -12, 40, 24); // Shadow
+  
+    // // Moon
+    // ctx.save();
+    // ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+    // ctx.translate(0, 28.5);
+    // ctx.restore();
+  
+    // ctx.restore();
+  
+    // ctx.beginPath();
+    // ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+    // ctx.stroke();
+  
+    //window.requestAnimationFrame(draw);
     
 
     // todo2 wyszarzanie buttona
