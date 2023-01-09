ad829981430b9741ceb7c744046c36d3e689eca6
murasame112 <79984587+murasame112@users.noreply.github.com>
2023-01-09 13:42:55 +0100

    tier1 p8

--- projekt8/script.js
+++ projekt8/script.js
@@ -11,21 +11,25 @@ const minWidth = 0;
 const minHeight = minWidth;
 // =====
 
-let xValue=0;
+let xValue = 0;
+let yValue = 200;
 let animation;
-let balls =[];
+let balls = [];
+let lines = [];
 
 const drawButton = document.querySelector('#draw');
 const initButton = document.querySelector('#init');
 const xValueInput = document.querySelector('#xValue');
+const yValueInput = document.querySelector('#yValue');
 xValueInput.addEventListener('change', setDraw);
+yValueInput.addEventListener('change', setDraw);
 drawButton.addEventListener('click', executeDraw);
 initButton.disabled = true; 
 drawButton.disabled = true; 
 
 // funkcja ustawia przycisk 'set' na mozliwy do klikniecia, gdy jest jakas wartosc w inpucie
 function setDraw(){
-    if(xValueInput.value.length != 0){
+    if((xValueInput.value.length != 0) && (yValueInput.value.length != 0)){
         initButton.disabled = false;
         initButton.addEventListener('click', init);
     }else{
@@ -49,6 +53,7 @@ class Ball {
     createBall(){
         if (canvas.getContext) {
             ctx.beginPath();
+            ctx.lineWidth = 6;
             ctx.arc(this.xPosition, this.yPosition, this.radius, 0, 2 * Math.PI);
             ctx.fillStyle = '#49d169';
             ctx.fill();
@@ -87,22 +92,88 @@ class Ball {
     }
 }
 
+class Line {
+    constructor(moveToX, moveToY, lineToX, lineToY){
+        this.moveToX = moveToX;
+        this.moveToY = moveToY;
+        this.lineToX = lineToX;
+        this.lineToY = lineToY;
+    }
+
+    createLine(){
+        ctx.beginPath();
+        ctx.moveTo(this.moveToX,this.moveToY);
+        ctx.lineTo(this.lineToX,this.lineToY);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    draw(){
+        this.createLine();
+    }
+}
+
+function lineCheck(){
+
+    for(let i= 0 ; i<balls.length;i++)
+    {
+
+        for(let j =0;j<balls.length;j++)
+        {
+            
+            if(balls[i].xPosition != balls[j].xPosition && balls[i].yPosition != balls[j].yPosition )
+            {
+                let xA = 0;
+                let xB = 0;
+                let yA = 0;
+                let yB = 0;
+
+                xA = balls[i].xPosition;
+                xB = balls[j].xPosition;
+                yA = balls[i].yPosition;
+                yB = balls[j].yPosition;
+
+                const pythagoreanA = Math.pow((xA - xB),2);
+                const pythagoreanB = Math.pow((yA - yB),2);
+                const pythagoreanC = Math.sqrt(pythagoreanA + pythagoreanB);
+
+
+                if(pythagoreanC <= yValue)
+                {
+                    let line = new Line(xA,yA,xB,yB)
+                    lines.push(line);
+
+                    lines.forEach(element =>{
+                        element.draw();
+                    })
+
+                }
+                
+                
+                   
+            }
+            lines = [];
+        }
+    }
+
+}
+
 
 function init(){
     balls =[];
     xValue = xValueInput.value;
-    console.log(xValue);
+    yValue = yValueInput.value;
     xValueInput.value = null;
     initButton.disabled = true; 
 
     for(let i = 0; i < xValue; i++)
     {
     
-        const radius = Math.floor(Math.random() * 20) + 10;
-        const speed = -radius+30;
-        const xPosition = Math.floor(Math.random() * (maxWidth - radius-1)) + radius+1;
-        const yPosition = Math.floor(Math.random() * (maxHeight - radius-1)) + radius+1;
-
+        let radius = Math.random() * (20 - 10) + 10;
+        const speed = (-radius+21)/2;
+        radius = radius*(1.5);
+        const xPosition = Math.random() * ((maxWidth - radius)-radius) + radius;
+        const yPosition = Math.random() * ((maxHeight - radius)-radius) + radius;
         const ball = new Ball(xPosition, yPosition, radius, speed);
         balls.push(ball);
         drawButton.disabled = false; 
@@ -115,6 +186,12 @@ function executeDraw(){
     drawButton.disabled = true; 
     animation = requestAnimationFrame(executeDraw);
     ctx.clearRect(0, 0, maxWidth, maxHeight);
+
+    lineCheck();
+    lines.forEach(l =>{
+        l.draw();
+        
+    })
 
     balls.forEach(element =>{
         element.draw();
